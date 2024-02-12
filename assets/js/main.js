35cdf06d825b51a9aee88959f21514a6838b99b2
rukhsar624 <rukhsaranwar56@gmail.com>
2024-02-12 17:52:24 +0500

    add all files

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('scroll', function () {
+    var headerSecond = document.getElementById('header-second');
+
+    // Adjust the scroll threshold as needed
+    var scrollThreshold = 100;
+
+    if (window.scrollY > scrollThreshold) {
+        headerSecond.style.display = 'block';
+    } else {
+        headerSecond.style.display = 'none';
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    var searchInputsContainer = document.getElementById('searchInputsContainer');
+    var openDiv = document.querySelector('.open-div');
+
+    searchInputsContainer.addEventListener('click', function() {
+      openDiv.classList.toggle('open');
+    });
+  });
+//   
+
