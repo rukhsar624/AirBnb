caa77e7968c48346bebb1698455ba0146fb8e3c9
rukhsar624 <rukhsaranwar56@gmail.com>
2024-02-15 18:00:43 +0500

    add today 2/15/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,140 +1,140 @@
 // 
 window.addEventListener('scroll', function () {
-    var headerSecond = document.getElementById('header-second');
-    // Adjust the scroll threshold as needed
-    var scrollThreshold = 100;
+	var headerSecond = document.getElementById('header-second');
+	// Adjust the scroll threshold as needed
+	var scrollThreshold = 100;
 
-    if (window.scrollY > scrollThreshold) {
-        headerSecond.style.display = 'block';
-    } else {
-        headerSecond.style.display = 'none';
-    }
+	if (window.scrollY > scrollThreshold) {
+		headerSecond.style.display = 'block';
+	} else {
+		headerSecond.style.display = 'none';
+	}
 });
 // 
 // 
-document.addEventListener('DOMContentLoaded', function() {
-    var searchInputsContainer = document.getElementById('searchInputsContainer');
-    var openDiv = document.querySelector('.open-div');
+document.addEventListener('DOMContentLoaded', function () {
+	var searchInputsContainer = document.getElementById('searchInputsContainer');
+	var openDiv = document.querySelector('.open-div');
 
-    searchInputsContainer.addEventListener('click', function() {
-      openDiv.classList.toggle('open');
-    });
-  });
+	searchInputsContainer.addEventListener('click', function () {
+		openDiv.classList.toggle('open');
+	});
+});
 //  
 
 // checkout
-document.addEventListener('DOMContentLoaded', function() {
-  var searchInputsContainer = document.getElementById('searchInputsContainerSecond');
-  var openDiv = document.querySelector('.open-div-second');
+document.addEventListener('DOMContentLoaded', function () {
+	var searchInputsContainer = document.getElementById('searchInputsContainerSecond');
+	var openDiv = document.querySelector('.open-div-second');
 
-  searchInputsContainer.addEventListener('click', function() {
-    openDiv.classList.toggle('open-second');
-  });
-}); 
+	searchInputsContainer.addEventListener('click', function () {
+		openDiv.classList.toggle('open-second');
+	});
+});
 // 
-document.addEventListener('DOMContentLoaded', function() {
-  var searchInputsContainer = document.getElementById('searchInputsContainerthird');
-  var openDiv = document.querySelector('.open-div-third');
+document.addEventListener('DOMContentLoaded', function () {
+	var searchInputsContainer = document.getElementById('searchInputsContainerthird');
+	var openDiv = document.querySelector('.open-div-third');
 
-  searchInputsContainer.addEventListener('click', function() {
-    openDiv.classList.toggle('open-third');
-  });
-}); 
+	searchInputsContainer.addEventListener('click', function () {
+		openDiv.classList.toggle('open-third');
+	});
+});
 // 
 // 
-document.addEventListener('DOMContentLoaded', function() {
-  var searchInputsContainer = document.getElementById('searchInputsContainerfourth');
-  var openDiv = document.querySelector('.open-div-forth');
+document.addEventListener('DOMContentLoaded', function () {
+	var searchInputsContainer = document.getElementById('searchInputsContainerfourth');
+	var openDiv = document.querySelector('.open-div-forth');
 
-  searchInputsContainer.addEventListener('click', function() {
-    openDiv.classList.toggle('open-forth');
-  });
-}); 
+	searchInputsContainer.addEventListener('click', function () {
+		openDiv.classList.toggle('open-forth');
+	});
+});
 // 
 // checkout
 // counter js open
-$(document).ready(function() {
-  const minus = $('.quantity__minus');
-  const plus = $('.quantity__plus');
-  const input = $('.quantity__input');
-  minus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    if (value > 1) {
-      value--;
-    }
-    input.val(value);
-  });
-  
-  plus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    value++;
-    input.val(value);
-  })
+$(document).ready(function () {
+	const minus = $('.quantity__minus');
+	const plus = $('.quantity__plus');
+	const input = $('.quantity__input');
+	minus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		if (value > 1) {
+			value--;
+		}
+		input.val(value);
+	});
+
+	plus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		value++;
+		input.val(value);
+	})
 });
 // 
-$(document).ready(function() {
-  const minus = $('.quantity__minus1');
-  const plus = $('.quantity__plus1');
-  const input = $('.quantity__input1');
-  minus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    if (value > 1) {
-      value--;
-    }
-    input.val(value);
-  });
-  
-  plus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    value++;
-    input.val(value);
-  })
+$(document).ready(function () {
+	const minus = $('.quantity__minus1');
+	const plus = $('.quantity__plus1');
+	const input = $('.quantity__input1');
+	minus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		if (value > 1) {
+			value--;
+		}
+		input.val(value);
+	});
+
+	plus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		value++;
+		input.val(value);
+	})
 });
 // 
 // 
-$(document).ready(function() {
-  const minus = $('.quantity__minus2');
-  const plus = $('.quantity__plus2');
-  const input = $('.quantity__input2');
-  minus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    if (value > 1) {
-      value--;
-    }
-    input.val(value);
-  });
-  
-  plus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    value++;
-    input.val(value);
-  })
+$(document).ready(function () {
+	const minus = $('.quantity__minus2');
+	const plus = $('.quantity__plus2');
+	const input = $('.quantity__input2');
+	minus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		if (value > 1) {
+			value--;
+		}
+		input.val(value);
+	});
+
+	plus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		value++;
+		input.val(value);
+	})
 });
-$(document).ready(function() {
-  const minus = $('.quantity__minus3');
-  const plus = $('.quantity__plus3');
-  const input = $('.quantity__input3');
-  minus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    if (value > 1) {
-      value--;
-    }
-    input.val(value);
-  });
-  
-  plus.click(function(e) {
-    e.preventDefault();
-    var value = input.val();
-    value++;
-    input.val(value);
-  })
+$(document).ready(function () {
+	const minus = $('.quantity__minus3');
+	const plus = $('.quantity__plus3');
+	const input = $('.quantity__input3');
+	minus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		if (value > 1) {
+			value--;
+		}
+		input.val(value);
+	});
+
+	plus.click(function (e) {
+		e.preventDefault();
+		var value = input.val();
+		value++;
+		input.val(value);
+	})
 });
 // 
 // counter js closed
@@ -195,7 +195,7 @@ class RangeSlider {
 		}
 
 		/* wrapper */
-		if (!isMulti) { 
+		if (!isMulti) {
 			range.parentNode.insertBefore(this.wrapper, range);
 			this.wrapper.appendChild(range);
 		}
@@ -212,7 +212,8 @@ class RangeSlider {
 			const svg = `
 				<svg class="${this.settings.clsRangeTicks}" width="100%" height="100%">
 				${ticks.map((index) => {
-					return `<rect x="${(100 / this.ticks) * index}%" y="5" width="1" height="100%"></rect>`}).join('')
+				return `<rect x="${(100 / this.ticks) * index}%" y="5" width="1" height="100%"></rect>`
+			}).join('')
 				}
 				<rect x="100%" y="5" width="1" height="100%"></rect>
 			</svg>`;
@@ -226,19 +227,19 @@ class RangeSlider {
 			this.setCenter();
 			this.output.setAttribute('tabindex', 0);
 			this.output.addEventListener('keydown', (event) => {
-				switch(event.key) {
+				switch (event.key) {
 					case 'ArrowLeft': case 'ArrowDown': event.preventDefault(); this.range.stepDown(); this.updateCircle(); break;
 					case 'ArrowRight': case 'ArrowUp': event.preventDefault(); this.range.stepUp(); this.updateCircle(); break;
 					default: break;
 				}
 			});
-			this.output.addEventListener('pointerdown', () => {return this.output.addEventListener('pointermove', pointerMove)});
-			this.output.addEventListener('pointerup', () => {return this.output.removeEventListener('pointermove', pointerMove)});
+			this.output.addEventListener('pointerdown', () => { return this.output.addEventListener('pointermove', pointerMove) });
+			this.output.addEventListener('pointerup', () => { return this.output.removeEventListener('pointermove', pointerMove) });
 
 			this.updateCircle();
 		}
 		else {
-			range.addEventListener('input', () => {return this.updateRange()});
+			range.addEventListener('input', () => { return this.updateRange() });
 		}
 
 		/* TODO: Send init event ? */
@@ -275,10 +276,10 @@ class RangeSlider {
 	updateCircle(start) {
 		let angle = start;
 		let rad = 360 / (this.max - this.min);
-		if (!angle) {angle = rad * this.range.valueAsNumber + this.settings.offset;}
+		if (!angle) { angle = rad * this.range.valueAsNumber + this.settings.offset; }
 		let end = angle - this.settings.offset;
-		if (end < 0) {end = end + 360;}
-		if (start) {this.range.value = Math.ceil(end / rad);}
+		if (end < 0) { end = end + 360; }
+		if (start) { this.range.value = Math.ceil(end / rad); }
 		this.wrapper.dataset.value = this.range.value;
 		this.wrapper.style.setProperty('--angle', `${angle}deg`);
 		this.wrapper.style.setProperty('--gradient-end', `${end}deg`);
@@ -305,7 +306,7 @@ class RangeSlider {
 		const value = (this.range.valueAsNumber - this.min) * this.multiplier;
 		this.range.style.setProperty(this.settings.varPercent, `${value}%`);
 		this.range.style.setProperty(this.settings.varValue, `${this.range.valueAsNumber}`);
-		
+
 		if (this.lower) {
 			this.lower.style.setProperty(this.settings.varPercentUpper, `${value}%`);
 		}
@@ -329,17 +330,90 @@ function stringToType(obj) {
 }
 
 function uuid() {
-	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-		{return (
+	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => {
+		return (
 			c ^
 			(crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-		).toString(16)}
+		).toString(16)
+	}
 	);
 }
 
 /* Demo: Run it */
 const elements = document.querySelectorAll('[data-range]');
-	elements.forEach(element => {
-		new RangeSlider(element, element.dataset);
-	})
+elements.forEach(element => {
+	new RangeSlider(element, element.dataset);
+})
 // Range slider js closed
+// swiper slider js
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	autoplay:
+	{
+		delay: 2000,
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		375: {
+			slidesPerView: 1,
+			spaceBetween: 50
+		},
+		1920: {
+			slidesPerView: 4,
+			spaceBetween: 20
+		},
+		1600: {
+			slidesPerView: 4,
+			spaceBetween: 20
+
+		},
+		1400: {
+			slidesPerView: 4,
+			spaceBetween: 20
+		},
+		358: {
+			slidesPerView: 1,
+			spaceBetween: 20
+
+		},
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 150
+
+		},
+		820: {
+			slidesPerView: 2,
+			spaceBetween: 20
+		},
+		1366: {
+			slidesPerView: 4,
+			spaceBetween: 30
+
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 10
+
+		},
+		1440:{
+			slidesPerView:12,
+			spaceBetween:10
+		}
+	}
+});
+// 
